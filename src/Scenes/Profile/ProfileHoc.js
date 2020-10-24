745402b83598988dde668f53ea512bfa16baec3a
Dagorik <dagorik@gmail.com>
2020-10-24 01:49:30 -0500

    fix eslint

--- src/Scenes/Profile/ProfileHoc.js
+++ src/Scenes/Profile/ProfileHoc.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default (Profile) => function Profilehoc() {
-    return (<Profile firstName="Primer Nombre" email="prueba@facebook.com" photoUrl={"https://i.stack.imgur.com/l60Hf.png"} />)
+    return (
+        <Profile
+            firstName="Primer Nombre"
+            email="prueba@facebook.com" 
+            photoUrl={"https://i.stack.imgur.com/l60Hf.png"} 
+        />)
 }
